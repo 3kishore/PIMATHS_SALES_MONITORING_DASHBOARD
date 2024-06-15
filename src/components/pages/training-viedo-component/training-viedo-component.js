dcf20c6dc533b797721eca8f96a34e2024091a58
Kishore Kuppan <kkuppan@vmware.com>
2024-06-15 12:02:07 +0530

    Joly O jymkhana

--- src/components/pages/training-viedo-component/training-viedo-component.js
+++ src/components/pages/training-viedo-component/training-viedo-component.js
@@ -3,7 +3,6 @@ import YouTube from "react-youtube";
 import { ApiServiceHelper } from "../../../services/api/api.service";
 import { APP } from "../../../services/utilities/APP.constant";
 import ErrorPageComponent from "../error-page/error-page.component";
-import trainingSession1 from '../../../assets/video/video.mov'; 
 
 function TrainingViedoComponent() {
 
@@ -17,13 +16,13 @@ function TrainingViedoComponent() {
 
   function getMyTrainingSessions() {
     _apiHelper.getMyTrainingSession().then(resp => {
-      // resp = {
-      //   data: {
-      //     status: true,
-      //     message: 'Success',
-      //     content: ['PhxfspwMdww', '7YpB7suzrto']
-      //   }
-      // }
+      resp = {
+        data: {
+          status: true,
+          message: 'Success',
+          content: ['PhxfspwMdww', '7YpB7suzrto']
+        }
+      }
       if(resp?.data?.status) {
         setTrainingSessions(resp?.data?.content || []);
         setIsLoading(false);
@@ -45,10 +44,12 @@ function TrainingViedoComponent() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // const videoSrc = "https://drive.google.com/uc?export=download&id=1vPlTuW5TyvQPL3SKn_BHixEvG6wcY-0f";
+
   return (
     <div className="w-full m-5">
       <h2 className="text-2xl font-medium mb-3">{APP.TRAINING_VIEDO_TITLE}</h2>
-      {/* {
+      {
         !isLoading ?
           !isErrorOccured ?
             <div className="flex flex-col gap-3">
@@ -60,14 +61,15 @@ function TrainingViedoComponent() {
               }
             </div> : <ErrorPageComponent />
           : <div className="h-80 bg-neutral-7 flex justify-center items-center">Loading...</div>
-      } */}
-      <video controls width="800" height="600">
-        <source src={trainingSession1} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      }
 
-      {/* <video controls="controls" width="800" height="600" name="Video Name">
-        <source src="../../../assets/viedo/video.mov" />
+        {/* <video controls width="800" height="600">
+          <source src={videoSrc} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video> */}
+      {/* <video controls width="800" height="600">
+        <source src={'https://drive.google.com/file/d/1vPlTuW5TyvQPL3SKn_BHixEvG6wcY-0f'} type="video/mp4" />
+        Your browser does not support the video tag.
       </video> */}
       
     </div>
